d7a5a75df09de8e081d8f532ebf1a900b8e644e4
divaymohan <divaymohan05@gmail.com>
2020-05-15 08:22:01 +0530

    update npm package and republish it

--- Day4/fistModule/index.js
+++ Day4/fistModule/index.js
@@ -1,3 +1,6 @@
 module.exports.add = function(a,b){
     return a+b;
 }
+module.exports.multiply = function(a,b){
+    return a*b;
+}
--- Day4/npm-demo/index.js
+++ Day4/npm-demo/index.js
@@ -7,3 +7,4 @@ var dm = require('dm-add');
 var res = _.contains([1,2,3],3);
 console.log(res);
 console.log(dm.add(1,2));
+console.log(dm.multiply(3,4));
