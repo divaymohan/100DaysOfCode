91fa366b7a50b1a786705232b4d331d1a69cc5f3
divaymohan <divaymohan05@gmail.com>
2020-05-15 08:10:07 +0530

    upto publishing of npm package

--- Day4/npm-demo/index.js
+++ Day4/npm-demo/index.js
@@ -1,8 +1,9 @@
 var _ = require('underscore');
-
+var dm = require('dm-add');
 //Core Module
 //File or Folder4
 // node_modules
 
 var res = _.contains([1,2,3],3);
 console.log(res);
+console.log(dm.add(1,2));
