0b24304719d8f4bfda7a74dd9eca70acad2eb586
divaymohan <divaymohan05@gmail.com>
2020-05-17 15:51:56 +0530

    Differentiate between env

--- Day7/userBackEnd/index.js
+++ Day7/userBackEnd/index.js
@@ -1,3 +1,5 @@
+const morgan = require('morgan');
+const helet = require('helmet');
 const express = require('express');
 const Joi = require('joi');
 const logger = require('./logger');
@@ -8,6 +10,16 @@ const app = express();
 app.use(express.json()); //req.body
 app.use(express.urlencoded());
 app.use(express.static('public'));
+app.use(helet());
+
+//how to eneble something in developement environment
+if(app.get('env') === 'development'){
+    console.log('welcome to development enviroment..!!');
+    console.log('enebling morgan to simply print the requist..!!');
+    app.use(morgan('tiny'));
+    console.log('morgan enebled..!');
+}
+
 //customized middleware
 app.use(logger);
 app.use(auth);
