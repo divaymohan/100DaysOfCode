2d4affdec2f3c4f9e0d443ebb186130e6556905d
divaymohan <divaymohan05@gmail.com>
2020-06-04 13:40:38 +0530

    adding style dynamically

--- Day_23/myappp/src/components/counter.jsx
+++ Day_23/myappp/src/components/counter.jsx
@@ -4,11 +4,17 @@ class Counter extends Component {
   state = {
     count: 0,
   };
+  style = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button className="btn">Increment</button>
+        <span style={this.style} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
