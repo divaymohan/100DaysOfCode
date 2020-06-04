24c671364d4d27733db4f3e6ff52bab2e592d90c
divaymohan <divaymohan05@gmail.com>
2020-06-04 13:46:27 +0530

    adding classes dynamically

--- Day_23/myappp/src/components/counter.jsx
+++ Day_23/myappp/src/components/counter.jsx
@@ -4,20 +4,21 @@ class Counter extends Component {
   state = {
     count: 0,
   };
-  style = {
-    fontSize: 10,
-    fontWeight: "bold",
-  };
+
   render() {
     return (
       <div>
-        <span style={this.style} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
