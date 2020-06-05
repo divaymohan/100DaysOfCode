e8285aae51fb89ebe2345a8a73d91e2bdef54354
divaymohan <divaymohan05@gmail.com>
2020-06-05 13:30:05 +0530

    added new component counters

---
+++ Day_23/myappp/src/components/counters.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Counter />
+        <Counter />
+        <Counter />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counters;
