8db2a8fd1d2d6d6dd99dacc74626ec15fc50bb04
divaymohan <divaymohan05@gmail.com>
2020-06-05 16:20:35 +0530

    before removing local state from counter

--- Day_23/myappp/src/components/counters.jsx
+++ Day_23/myappp/src/components/counters.jsx
@@ -15,9 +15,22 @@ class Counters extends Component {
     const counters = this.state.counters.filter((c) => c.id != counterId);
     this.setState({ counters });
   };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <React.Fragment>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
