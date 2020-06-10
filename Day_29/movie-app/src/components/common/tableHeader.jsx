0a1ff412285c40edb92964cff4b0bdc2cca1f367
divaymohan <divaymohan05@gmail.com>
2020-06-10 10:35:55 +0530

    extracting table header and table body

---
+++ Day_29/movie-app/src/components/common/tableHeader.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  state = {};
+  render() {
+    return <h1>Divay Mohan</h1>;
+  }
+}
+
+export default TableHeader;
