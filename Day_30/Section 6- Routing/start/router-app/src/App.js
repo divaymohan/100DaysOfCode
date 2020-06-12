bc8281efd3017d56d321989fcf6a2a0a87fd7680
divaymohan <divaymohan05@gmail.com>
2020-06-12 12:16:11 +0530

    implemented routing and nested routing

--- Day_30/Section 6- Routing/start/router-app/src/App.js
+++ Day_30/Section 6- Routing/start/router-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
@@ -16,15 +16,17 @@ class App extends Component {
         Home
         <NavBar />
         <Switch>
-          <Route path={"/Products/:id"} component={ProductDetails} />
+          <Route path="/Products/:id" component={ProductDetails} />
           <Route
-            path={"/Products"}
+            path="/Products"
             render={(props) => <Products sortBy={"newest"} {...props} />}
           />
-          <Route path={"/Posts/:year?/:month?"} component={Posts} />
-          <Route path={"/admin"} component={Dashboard} />
-          <Route path={"/ProductDetails"} component={ProductDetails} />
-          <Route path={"/"} component={Home} />
+          <Route path="/Posts/:year?/:month?" component={Posts} />
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/ProductDetails" component={ProductDetails} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/not-found" />
         </Switch>
       </div>
     );
--- Day_30/Section 6- Routing/start/router-app/src/components/admin/dashboard.jsx
+++ Day_30/Section 6- Routing/start/router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import SideBar from "./sideBar";
+
+import { Route } from "react-router-dom";
+import Posts from "./posts";
+import Users from "./users";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <SideBar />
+      <Route path="/admin/posts" component={Posts} />
+      <Route path="/admin/users" component={Users} />
     </div>
   );
 };
