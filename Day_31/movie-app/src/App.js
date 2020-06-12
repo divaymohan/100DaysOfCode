ab398dba46cfbb1d1a6d3dc409b100319f15e350
divaymohan <divaymohan05@gmail.com>
2020-06-12 15:10:22 +0530

    added nav bar component

--- Day_31/movie-app/src/App.js
+++ Day_31/movie-app/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
 
 function App() {
   return (
-    <main className="container">
-      <h1>
-        <span className="badge badge-dark text-success">movie app</span>{" "}
-      </h1>
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <h1>
+          <span className="badge badge-dark text-success">movie app</span>
+        </h1>
+        <Movies />
+      </main>
+    </React.Fragment>
   );
 }
 
---
+++ Day_31/movie-app/src/components/navBar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
