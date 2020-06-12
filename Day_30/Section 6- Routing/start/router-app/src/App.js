7a662f17c43eba3492c1478269aec30269cb5349
divaymohan <divaymohan05@gmail.com>
2020-06-12 09:54:54 +0530

    routing and navigations started

--- Day_30/Section 6- Routing/start/router-app/src/App.js
+++ Day_30/Section 6- Routing/start/router-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
+import { Route } from "react-router-dom";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
@@ -12,7 +13,13 @@ class App extends Component {
   render() {
     return (
       <div>
+        Home
         <NavBar />
+        <Route path={"/"} component={Home} />
+        <Route path={"/Products"} component={Products} />
+        <Route path={"/Posts"} component={Posts} />
+        <Route path={"/admin"} component={Dashboard} />
+        <Route path={"/ProductDetails"} component={ProductDetails} />
       </div>
     );
   }
